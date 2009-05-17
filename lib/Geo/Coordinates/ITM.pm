93ba14ba8b07cc4a1253245f7e1729ce37dbcd4a
Andy Armstrong <andy@hexten.net>
2009-05-17 19:56:30 +0000

    Added.

---
+++ lib/Geo/Coordinates/ITM.pm
@@ -0,0 +1,62 @@
+package Geo::Coordinates::ITM;
+
+use warnings;
+use strict;
+
+=head1 NAME
+
+Geo::Coordinates::ITM - [One line description of module's purpose here]
+
+=head1 VERSION
+
+This document describes Geo::Coordinates::ITM version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+=head1 SYNOPSIS
+
+    use Geo::Coordinates::ITM;
+  
+=head1 DESCRIPTION
+
+=head1 INTERFACE 
+
+=head2 C<< somfunc >>
+
+=cut
+
+1;
+__END__
+
+=head1 CONFIGURATION AND ENVIRONMENT
+  
+Geo::Coordinates::ITM requires no configuration files or environment variables.
+
+=head1 DEPENDENCIES
+
+None.
+
+=head1 INCOMPATIBILITIES
+
+None reported.
+
+=head1 BUGS AND LIMITATIONS
+
+No bugs have been reported.
+
+Please report any bugs or feature requests to
+C<bug-geo-coordinates-itm@rt.cpan.org>, or through the web interface at
+L<http://rt.cpan.org>.
+
+=head1 AUTHOR
+
+Andy Armstrong  C<< <andy@hexten.net> >>
+
+=head1 LICENCE AND COPYRIGHT
+
+Copyright (c) 2009, Andy Armstrong C<< <andy@hexten.net> >>.
+
+This module is free software; you can redistribute it and/or
+modify it under the same terms as Perl itself. See L<perlartistic>.
